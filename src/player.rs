42d9fc07a93b8cdca6c78817a286386b27c4a40f
Andrew Smith <andrewcs149@gmail.com>
2022-06-19 15:40:27 -0700

    Add sprite animation for idle hero.

--- src/player.rs
+++ src/player.rs
@@ -7,25 +7,57 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(spawn_player)
-            .add_system(physics::movement::<Player>);
+            .add_system(physics::movement::<Player>)
+            .add_system(animate_sprite);
     }
 }
 
-fn spawn_player(mut commands: Commands) {
-    let sprite = SpriteBundle {
-        sprite: Sprite {
-            color: Color::ORANGE,
-            custom_size: Some(Vec2::new(30.0, 30.0)),
+fn spawn_player(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let texture_handle = asset_server.load("heroSpriteSheet.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(35.0, 47.0), 3, 1);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    let sprite_sheet = SpriteSheetBundle {
+        sprite: TextureAtlasSprite {
+            custom_size: Some(Vec2::new(10.0, 15.0)),
             ..default()
         },
+        texture_atlas: texture_atlas_handle,
+        transform: Transform::from_scale(Vec3::splat(6.0)),
         ..default()
     };
 
     commands
-        .spawn_bundle(sprite)
+        .spawn_bundle(sprite_sheet)
+        .insert(AnimationTimer(Timer::from_seconds(0.1, true)))
         .insert(Player)
         .insert(Speed(150.0))
         .insert(Sprint(1.5))
         .insert(Direction::Down)
         .insert(IsSprinting(false));
+}
+
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
+fn animate_sprite(
+    time: Res<Time>,
+    texture_atlases: Res<Assets<TextureAtlas>>,
+    mut query: Query<(
+        &mut AnimationTimer,
+        &mut TextureAtlasSprite,
+        &Handle<TextureAtlas>,
+    )>,
+) {
+    for (mut timer, mut sprite, texture_atlas_handle) in query.iter_mut() {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
+            sprite.index = (sprite.index + 1) % texture_atlas.textures.len();
+        }
+    }
 }
