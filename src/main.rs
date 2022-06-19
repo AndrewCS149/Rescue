a9bbba110970e2a038bc718aa22357495715aabe
Andrew Smith <andrewcs149@gmail.com>
2022-06-19 00:16:52 -0700

    Add 2d camera

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,20 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
         .add_startup_system(spawn_player)
         .run();
+}
+
+fn setup(mut commands: Commands) {
+    let camera = OrthographicCameraBundle::new_2d();
+    commands.spawn_bundle(camera);
 }
 
 fn spawn_player(mut commands: Commands) {
     let sprite = SpriteBundle {
         sprite: Sprite {
-            color: Color::AZURE,
+            color: Color::ORANGE,
             custom_size: Some(Vec2::new(30.0, 30.0)),
             ..default()
         },
