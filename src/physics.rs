c50e650fd6581da94cb5d4f3d24f30829b41d763
Andrew Smith <andrewcs149@gmail.com>
2022-06-19 12:07:21 -0700

    restrict player from firing while sprinting

--- src/physics.rs
+++ src/physics.rs
@@ -1,14 +1,24 @@
-use crate::components::{Direction, Speed, Sprint};
+use crate::components::{Direction, IsSprinting, Speed, Sprint};
 use bevy::prelude::*;
 
 pub fn movement<T: Component>(
     keys: Res<Input<KeyCode>>,
     time: Res<Time>,
-    mut query: Query<(&mut Transform, &Speed, &Sprint, &mut Direction, With<T>)>,
+    mut query: Query<
+        (
+            &mut Transform,
+            &Speed,
+            &Sprint,
+            &mut Direction,
+            &mut IsSprinting,
+        ),
+        With<T>,
+    >,
 ) {
-    for (mut transform, speed, sprint, mut direction, _) in query.iter_mut() {
+    for (mut transform, speed, sprint, mut direction, mut is_sprinting) in query.iter_mut() {
         let mut new_pos = Vec3::new(0.0, 0.0, 0.0);
         let mut tmp_sprint = 1.0;
+        is_sprinting.0 = false;
 
         // left
         if keys.pressed(KeyCode::A) {
@@ -37,6 +47,7 @@ pub fn movement<T: Component>(
         // sprint
         if keys.pressed(KeyCode::LShift) {
             tmp_sprint = sprint.0;
+            is_sprinting.0 = true;
         }
 
         transform.translation += new_pos * speed.0 * tmp_sprint * time.delta_seconds();
--- src/projectile.rs
+++ src/projectile.rs
@@ -1,4 +1,4 @@
-use crate::components::{Direction, Player, Projectile, Speed};
+use crate::components::{Direction, IsSprinting, Player, Projectile, Speed};
 use bevy::prelude::*;
 
 pub struct ProjectilePlugin;
@@ -13,28 +13,27 @@ impl Plugin for ProjectilePlugin {
 fn spawn_projectile<T: Component>(
     mut commands: Commands,
     keys: Res<Input<KeyCode>>,
-    query: Query<(&Transform, &Direction), With<T>>,
+    query: Query<(&Transform, &Direction, &IsSprinting), With<T>>,
 ) {
-    // only if the player has pressed the fire (space) button
-    if keys.just_pressed(KeyCode::Space) {
-        let pos = query.single().0.translation;
-        let direction = query.single().1;
-
-        let sprite = SpriteBundle {
-            sprite: Sprite {
-                color: Color::RED,
-                custom_size: Some(Vec2::new(5.0, 5.0)),
+    for (player_transform, direction, is_sprinting) in query.iter() {
+        // only if the player has pressed the fire (space) button
+        if keys.just_pressed(KeyCode::Space) && !is_sprinting.0 {
+            let sprite = SpriteBundle {
+                sprite: Sprite {
+                    color: Color::RED,
+                    custom_size: Some(Vec2::new(5.0, 5.0)),
+                    ..default()
+                },
+                transform: *player_transform,
                 ..default()
-            },
-            transform: Transform::from_xyz(pos.x, pos.y, 0.0), // use players translation
-            ..default()
-        };
+            };
 
-        commands
-            .spawn_bundle(sprite)
-            .insert(Projectile)
-            .insert(*direction)
-            .insert(Speed(1000.0));
+            commands
+                .spawn_bundle(sprite)
+                .insert(Projectile)
+                .insert(*direction)
+                .insert(Speed(1000.0));
+        }
     }
 }
 
