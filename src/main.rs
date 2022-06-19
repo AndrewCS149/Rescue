c84229dae64dfed9fef71b1c66a6f4016b6ff2c7
Andrew Smith <andrewcs149@gmail.com>
2022-06-19 00:13:15 -0700

    Add spawn_player() function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(spawn_player)
+        .run();
+}
+
+fn spawn_player(mut commands: Commands) {
+    let sprite = SpriteBundle {
+        sprite: Sprite {
+            color: Color::AZURE,
+            custom_size: Some(Vec2::new(30.0, 30.0)),
+            ..default()
+        },
+        ..default()
+    };
+
+    commands.spawn_bundle(sprite);
 }
