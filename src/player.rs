4f716b330bf1ce3a9db33e6dd4b30ddf3938f2cc
Andrew Smith <andrewcs149@gmail.com>
2022-06-19 22:24:08 -0700

    Replace hero and remove idle animation

--- src/player.rs
+++ src/player.rs
@@ -14,20 +14,29 @@ impl Plugin for PlayerPlugin {
 fn spawn_player(
     mut commands: Commands,
     assets: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    // mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
-    let image = assets.load("hero/idle.png");
-    let atlas = TextureAtlas::from_grid(image, Vec2::new(35.0, 47.0), 3, 1);
-    let handle = texture_atlases.add(atlas);
+    // let image = assets.load("hero/idle.png");
+    // let atlas = TextureAtlas::from_grid(image, Vec2::new(35.0, 47.0), 3, 1);
+    // let handle = texture_atlases.add(atlas);
 
-    let sprite_sheet = SpriteSheetBundle {
-        texture_atlas: handle,
-        // transform: Transform::from_scale(Vec3::splat(6.0)),
+    // let sprite_sheet = SpriteSheetBundle {
+    //     texture_atlas: handle,
+    // transform: Transform::from_scale(Vec3::splat(6.0)),
+    // ..default()
+    // };
+
+    let sprite = SpriteBundle {
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(52.5, 70.5)),
+            ..default()
+        },
+        texture: assets.load("hero/idle.png"),
         ..default()
     };
 
     commands
-        .spawn_bundle(sprite_sheet)
+        .spawn_bundle(sprite)
         .insert(AnimationTimer(Timer::from_seconds(0.1, true)))
         .insert(Player)
         .insert(Speed(150.0))
