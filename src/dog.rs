610118b49b7689f23eb9ad15fb0a8930979c0071
Andrew Smith <andrewcs149@gmail.com>
2022-06-24 23:59:47 -0700

    add random dog

--- src/dog.rs
+++ src/dog.rs
@@ -1,7 +1,26 @@
 use bevy::prelude::*;
 
+const SIZE_X: f32 = 40.0;
+const SIZE_Y: f32 = 40.0;
+
 pub struct DogPlugin;
 
 impl Plugin for DogPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(spawn_dog);
+    }
+}
+
+fn spawn_dog(mut commands: Commands, assets: Res<AssetServer>) {
+    let dog = SpriteBundle {
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(SIZE_X, SIZE_Y)),
+            ..default()
+        },
+        transform: Transform::from_xyz(-200.0, -200.0, 1.0),
+        texture: assets.load("dog.png"),
+        ..default()
+    };
+
+    commands.spawn_bundle(dog);
 }
